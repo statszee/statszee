90dd96620937f01599e6e17dc70a5d3824d6ecb8
Mattia Lobertini <lobetia@gmail.com>
2017-02-21 17:42:15 +0100

    - trailpack-sequelize accept URI connection string

--- server.js
+++ server.js
@@ -1,21 +1,12 @@
 'use strict'
 
-const database     = process.env.DATABASE_URL;
-const databaseData = database.match(/(postgres:\/\/)([^:]+):([^@]+)@([^@]+):([0-9]+)\/(.+)/i)
-
 const statsZee         = require("./");
 const statsZeeInstance = new statsZee({
   osStats:  true,
   database: {
     stores: {
       database: {
-        host:     databaseData[4],
-        port:     databaseData[5],
-        database: databaseData[6],
-        username: databaseData[2],
-        password: databaseData[3],
-        native: true,
-        ssl: true
+        uri: process.env.DATABASE_URL
       }
     }
   }
